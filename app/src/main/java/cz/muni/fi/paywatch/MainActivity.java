9358edca0224db5201c6bda1e4ac9639f6798fe8
danielminarik <daniel.minarik@gmail.com>
2017-03-10 23:45:23 +0100

    created project in android studio

---
+++ app/src/main/java/cz/muni/fi/paywatch/MainActivity.java
@@ -0,0 +1,13 @@
+package cz.muni.fi.paywatch;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
