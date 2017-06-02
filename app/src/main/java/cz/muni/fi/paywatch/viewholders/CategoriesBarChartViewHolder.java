be4bc1e006c327f3839bb98079d68be806050a54
Jiří Doušek <jiri.dousek@gmail.com>
2017-06-02 14:48:58 +0200

    X axis labels

--- app/src/main/java/cz/muni/fi/paywatch/viewholders/CategoriesBarChartViewHolder.java
+++ app/src/main/java/cz/muni/fi/paywatch/viewholders/CategoriesBarChartViewHolder.java
@@ -8,6 +8,7 @@ import android.widget.TextView;
 
 import com.github.mikephil.charting.charts.BarChart;
 import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.components.AxisBase;
 import com.github.mikephil.charting.components.XAxis;
 import com.github.mikephil.charting.data.BarData;
 import com.github.mikephil.charting.data.BarDataSet;
@@ -15,6 +16,7 @@ import com.github.mikephil.charting.data.LineData;
 import com.github.mikephil.charting.data.LineDataSet;
 import com.github.mikephil.charting.data.realm.implementation.RealmBarDataSet;
 import com.github.mikephil.charting.data.realm.implementation.RealmLineDataSet;
+import com.github.mikephil.charting.formatter.IAxisValueFormatter;
 import com.jjoe64.graphview.GraphView;
 import com.jjoe64.graphview.series.DataPoint;
 import com.jjoe64.graphview.series.LineGraphSeries;
@@ -64,6 +66,8 @@ public class CategoriesBarChartViewHolder extends RecyclerView.ViewHolder {
         startDate = mAdapter.activity.getCurrentMonthStart();
         endDate = mAdapter.activity.getCurrentMonthEnd();
 
+        List<String> categoryNames = new ArrayList<>();
+        
         for (Category category : RealmController.with((Activity)itemView.getContext()).getCategories(Constants.CAT_TYPE_EXPENSE) ) {
              float sum = realm.where(Entry.class)
                      .equalTo("categoryId", category.getId())
@@ -72,6 +76,13 @@ public class CategoriesBarChartViewHolder extends RecyclerView.ViewHolder {
                      .equalTo("accountId", mAdapter.activity.getCurrentAccountId())
                      .sum("sum").floatValue() * (-1);
 
+
+             String name = category.getName();
+             if ( name.length() > 6 ) {
+                name = name.substring(0, 6) + ".";
+             }
+             categoryNames.add(name);
+
              entries.add(new com.github.mikephil.charting.data.BarEntry(category.getId().floatValue(), sum));
              prediction.add(new com.github.mikephil.charting.data.BarEntry(category.getId().floatValue(), getAverage(category.getId())));
 
@@ -79,14 +90,32 @@ public class CategoriesBarChartViewHolder extends RecyclerView.ViewHolder {
 
         BarDataSet dataSet = new BarDataSet(entries, "Reality"); // add entries to dataset
 
-        BarDataSet dataSet2 = new BarDataSet(prediction, "Prediction"); // add entries to dataset
+        BarDataSet dataSet2 = new BarDataSet(prediction, "Month average"); // add entries to dataset
         dataSet2.setColor(Color.RED);
 
+        final String[] labels = new String[categoryNames.size()];
+        categoryNames.toArray(labels);
+
+        IAxisValueFormatter formatter = new IAxisValueFormatter() {
+
+            @Override
+            public String getFormattedValue(float value, AxisBase axis) {
+                return labels[(int) value ];
+            }
+        };
 
         BarData data = new BarData(dataSet, dataSet2);
-        chart.getXAxis().setPosition(XAxis.XAxisPosition.BOTTOM);
+        data.setBarWidth(0.4f);
+
+        XAxis xAxis = chart.getXAxis();
+        xAxis.setGranularity(1f); // minimum axis-step (interval) is 1
+        xAxis.setValueFormatter(formatter);
+        xAxis.setPosition(XAxis.XAxisPosition.BOTTOM);
+        xAxis.setLabelCount(categoryNames.size());
+        xAxis.setLabelRotationAngle(45.0f);
+
         chart.setData(data);
-        chart.groupBars(0.0f, 0.03f, 0.08f);
+        chart.groupBars(-0.5f, 0.03f, 0.08f);
         chart.getDescription().setEnabled(false);
         chart.invalidate(); // refresh
 
